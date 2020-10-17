691f68a169b68df906bdd4cfa6afa63d9f05b1fd
musiccity365 <m_sackstein@yahoo.com>
2020-10-17 18:20:24 -0400

    --- Auto-Git Commit ---

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,38 @@
 require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
-  @@artists = []
+    @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+    def initialize
+      @@artists << self
+      @songs = []
+    end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+    def self.find_by_name(name)
+      @@artists.detect{|a| a.name == name}
+    end
 
-  def self.all
-    @@artists
-  end
+    def self.all
+      @@artists
+    end
 
-  def self.reset_all
-    self.all.clear
-  end
+    def add_song(song)
+      @songs << song
+      song.artist = self
+    end
 
-  def self.count
-    self.all.count
-  end
+    def add_songs(songs)
+      songs.each { |song| add_song(song) }
+    end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+    # def to_param
+    #   name.downcase.gsub(' ', '-')
+    # end
 end
